b9f5736552681e9d27e572123f84b80794fce833
phillip nzau junior <nzau78@gmail.com>
2022-03-22 17:38:44 +0300

    landing page setup

--- topUpMama/src/app/app-routing.module.ts
+++ topUpMama/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/auth/login/login.component";
 import {RegisterComponent} from "./components/auth/register/register.component";
+import {UsersPageComponent} from "./components/users-page/users-page.component";
+import {MyAccountPageComponent} from "./components/my-account-page/my-account-page.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'users', component: UsersPageComponent},
+  {path: 'my-account', component: MyAccountPageComponent},
+  {path: '**', component: LandingPageComponent},
+
 ];
 
 @NgModule({
--- topUpMama/src/app/app.module.ts
+++ topUpMama/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { MyAccountPageComponent } from './components/my-account-page/my-account-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LandingPageComponent,
+    UsersPageComponent,
+    MyAccountPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
