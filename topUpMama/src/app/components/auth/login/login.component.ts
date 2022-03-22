8199d48911915c7f894a99b02a90606d334502f0
phillip nzau junior <nzau78@gmail.com>
2022-03-22 10:15:23 +0300

    sort of validation

--- topUpMama/src/app/components/auth/login/login.component.ts
+++ topUpMama/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,12 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   isShowPassword = false;
+  submitted = false;
+  // Create the login form
+  loginForm = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,100}$')]]
+  });
 
-  constructor() {}
+  constructor(
+    private fb: FormBuilder
+  ) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   changeIsShowPassword() {
     this.isShowPassword = !this.isShowPassword;
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  loginUserSubmit() {
+    this.submitted = true;
+
+
+
+  }
+
+  // check form validity
+  isValid(fieldName: any): boolean {
+    return (
+      this.loginForm.controls[fieldName].invalid &&
+      (this.loginForm.controls[fieldName].dirty ||
+        this.loginForm.controls[fieldName].touched)
+    );
   }
 }
